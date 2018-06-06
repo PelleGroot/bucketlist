8469b6ead7b3a574125f330f3181bb9ff4784bc6
PelleGroot <pellegroot@gmail.com>
2018-06-06 16:21:15 +0200

    added bucketlistItem dataclass

---
+++ app/src/main/java/nl/pellegroot/bucketlist/bucketlistItem.java
@@ -0,0 +1,14 @@
+package nl.pellegroot.bucketlist;
+
+import android.location.Location;
+import android.media.Image;
+
+public class bucketlistItem {
+    public String name;
+    public String description;
+    public Location location;
+    public Image photo;
+    public Boolean activityDone;
+    public int rating;
+    public float price;
+}
